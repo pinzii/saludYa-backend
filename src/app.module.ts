fa5f8a3dafb92000efc8ce454ec2e1f142c2d820
pinzii <fpinzon514@gmail.com>
2026-05-26 04:43:09 -0500

    fix: corregir tipado de parseInt para DB_PORT en entornos estrictos

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,13 +10,13 @@ import { AppointmentsModule } from './appointments/appointments.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '31415926',
-      database: 'salud_ya',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '', 10) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || '31415926',
+      database: process.env.DB_DATABASE || 'salud_ya',
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: true, // Mantenlo así para que cree las tablas automáticamente en Render
     }),
     UsersModule,
     AuthModule,
